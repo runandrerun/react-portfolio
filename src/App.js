1012f3ba7aa14f9900c5da3103e023c90a974bb1
runandrerun <the.asantiagojr@gmail.com>
2018-10-10 16:57:37 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import Resume from './components/resume/resume';
 import Portfolio from './components/portfolio/portfolio';
 import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
+import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
-        <Header />
-        <About />
-        <Resume />
-        <Portfolio />
-        <ContactUs />
-        <Footer />
+        <Route exact path={`/`} render={() =>
+          <React.Fragment>
+          <Header />
+          <About />
+          <Resume />
+          <Portfolio />
+          <ContactUs />
+          <Footer />
+          </React.Fragment>
+        } />
+
       </div>
     );
   }
 }
-export default App;
+export default withRouter(App);
